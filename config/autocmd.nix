ad92e3e91ac358ef5bbe08dc86dc4c1832c3a8fd
oxygen <ozz.gaming@proton.me>
2024-06-23 08:12:57 +0200

    update

---
+++ config/autocmd.nix
@@ -0,0 +1,159 @@
+{  self, ... }:
+{
+  config = {
+    autoGroups = {
+      general = {
+        clear = true;
+      };
+    };
+
+    autoCmd = [
+      {
+        event = "VimEnter";
+        command = ''
+          local directory = vim.fn.isdirectory(data.file) == 1
+          if directory then
+            vim.cmd.cd(data.file)
+            require("nvim-tree.api").tree.open()
+          end
+        '';
+        group = "general";
+        desc = "Open NvimTree when it's a Directory";
+      }
+      {
+        event = "BufEnter";
+        pattern = "*.norg";
+        command = "norm zR";
+        group = "general";
+        desc = "Unfold when entering .norg files";
+      }
+      {
+        event = "TermOpen";
+        pattern = "*";
+        command = "startinsert";
+      }
+      {
+        event = ["BufReadPost", "BufNewFile"];
+        once = true;
+        command = ''
+          if vim.fn.has "wsl" == 1 then
+            vim.g.clipboard = {
+              copy = {
+                ["+"] = "win32yank.exe -i --crlf",
+                ["*"] = "win32yank.exe -i --crlf",
+              },
+              paste = {
+                ["+"] = "win32yank.exe -o --lf",
+                ["*"] = "win32yank.exe -o --lf",
+              },
+            }
+          end
+          vim.opt.clipboard = "unnamedplus"
+        '';
+        group = "general";
+        desc = "Lazy load clipboard";
+      }
+      {
+        event = "TermOpen";
+        command = ''
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+          vim.cmd "startinsert!"
+        '';
+        group = "general";
+        desc = "Terminal Options";
+      }
+      {
+        event = "BufReadPost";
+        command = ''
+          if vim.fn.line("'\"") > 1 && vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.cmd "normal! g`\""
+          end
+        '';
+        group = "general";
+        desc = "Go To The Last Cursor Position";
+      }
+      {
+        event = "TextYankPost";
+        command = ''
+          require("vim.highlight").on_yank { higroup = "YankHighlight", timeout = 200 }
+        '';
+        group = "general";
+        desc = "Highlight when yanking";
+      }
+      {
+        event = "BufEnter";
+        command = ''
+          vim.opt.formatoptions:remove { "c", "r", "o" }
+        '';
+        group = "general";
+        desc = "Disable New Line Comment";
+      }
+      {
+        event = "FileType";
+        pattern = ["c", "cpp", "py", "java", "cs"];
+        command = ''
+          vim.bo.shiftwidth = 4
+        '';
+        group = "general";
+        desc = "Set shiftwidth to 4 in these filetypes";
+      }
+      {
+        event = "FocusGained";
+        command = ''
+          vim.cmd "checktime"
+        '';
+        group = "general";
+        desc = "Update file when there are changes";
+      }
+      {
+        event = "VimResized";
+        command = ''
+          vim.cmd "wincmd ="
+        '';
+        group = "general";
+        desc = "Equalize Splits";
+      }
+      {
+        event = "ModeChanged";
+        command = ''
+          if vim.fn.getcmdtype() == "/" || vim.fn.getcmdtype() == "?" then
+            vim.opt.hlsearch = true
+          else
+            vim.opt.hlsearch = false
+          end
+        '';
+        group = "general";
+        desc = "Highlighting matched words when searching";
+      }
+      {
+        event = "FileType";
+        pattern = ["gitcommit", "markdown", "text", "log"];
+        command = ''
+          vim.opt_local.wrap = true
+          vim.opt_local.spell = false
+        '';
+        group = "general";
+        desc = "Enable Wrap in these filetypes";
+      }
+      {
+        event = "BufWritePost";
+        pattern = "*.vim";
+        command = ''
+          vim.cmd "source <afile>"
+        '';
+        group = "general";
+        desc = "Automatically source vim file whenever you save it";
+      }
+      {
+        event = "BufWritePost";
+        pattern = "~/.config/nvim/lua/theme/init.lua";
+        command = ''
+          vim.cmd("lua require('utils').CheckAndSetColorscheme()")
+        '';
+        group = "general";
+        desc = "Watch Lua File for Changes and Update Colorscheme";
+      }
+    ];
+  };
+}
--- config/simple-plugins.nix
+++ config/simple-plugins.nix
@@ -1,3 +1,4 @@
+{ pkgs, ...}:
 {
   plugins = {
     cmp.enable = true;
@@ -18,7 +19,7 @@
     headlines.enable = true;
     hmts.enable = true;
     image.enable = true;
-    indent-blankline.enable = true;
+    # indent-blankline.enable = true;
     lazy.enable = true;
     lazygit.enable = true;
     lsp.enable = true;
@@ -27,7 +28,34 @@
     luasnip.enable = true;
     markdown-preview.enable = true;
     nix.enable = true;
-    none-ls.enable = true;
+    # none-ls.enable = true;
+    none-ls = {
+      enable = true;
+      sources = {
+        formatting = {
+          # stylua.enable = true;
+          # google_java_format.enable = true;
+          # shfmt.enable = true;
+          # clang_format.enable = true;
+          stylelint.enable = true;
+	  # prettier = {
+	  #     enable = true;
+	  #     withArgs = " filetypes = { \"html\", \"markdown\", \"css\" }";
+	  # };
+        };
+        diagnostics = {
+          stylelint.enable = true;
+          # shellcheck = {
+          #   enable = true;
+          #   diagnostics_format = "#{m} [#{c}]";
+          # };
+        };
+        # code_actions = {
+        #   eslint.enable = true;
+        # };
+       };
+      # debug = true;
+    };
     notify.enable = true;
     nvim-autopairs.enable = true;
     nvim-colorizer.enable = true;
@@ -35,7 +63,75 @@
     oil.enable = true;
     plantuml-syntax.enable = true;
     surround.enable = true;
-    telescope.enable = true;
+    telescope = {
+      enable = true;
+      extensions = {
+        file-browser.enable = true;
+        frecency.enable = true;
+      };
+      settings = {
+        defaults = {
+          vimgrep_arguments = [
+            "rg"
+            "-L"
+            "--color=never"
+            "--no-heading"
+            "--with-filename"
+            "--line-number"
+            "--column"
+            "--smart-case"
+          ];
+          extensions = {
+            file_browser = {
+              path = ":%:p:h";
+              cwd_to_path = true;
+              initial_mode = "normal";
+            };
+          };
+          layout_config = {
+            prompt_position = "top";
+          };
+          mappings = {
+            i = {
+              "<A-j>" = {
+                __raw = "require('telescope.actions').move_selection_next";
+              };
+              "<A-k>" = {
+                __raw = "require('telescope.actions').move_selection_previous";
+              };
+            };
+          };
+          selection_caret = "  ";
+          prompt_prefix = "   ";
+
+          entry_prefix = "  ";
+          initial_mode = "insert";
+          selection_strategy = "reset";
+          sorting_strategy = "ascending";
+          layout_strategy = "horizontal";
+          layout_config = {
+            horizontal = {
+              prompt_position = "top";
+              preview_width = 0.55;
+              results_width = 0.8;
+            };
+            vertical = {
+              mirror = false;
+            };
+            width = 0.87;
+            height = 0.80;
+            preview_cutoff = 120;
+          };
+          set_env = {
+            COLORTERM = "truecolor";
+          };
+          winblend = 0;
+          border = {};
+          borderchars = [ "─" "│" "─" "│" "╭" "╮" "╯" "╰" ];
+          color_devicons = true;
+        };
+      };
+    };
     toggleterm.enable = true;
     transparent.enable = true;
     treesitter.enable = true;
@@ -43,4 +139,9 @@
     undotree.enable = true;
     which-key.enable = true;
   };
+
+  extraPlugins = with pkgs.vimPlugins; [
+    vim-rooter
+  ];
+  extraConfigLua = "vim.g.rooter_patterns = {'>.config', '.git', '.ozz', 'index.md', '>Documents', '>rPiPico', '>Pico', '>dotfiles', '=home-manager', '=nixos', '*.norg'}";
 }
