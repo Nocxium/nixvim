21a5d5d3abb3ea99d53ea40fa9619b4598b0c613
oxygen <ozz.gaming@proton.me>
2024-06-23 02:56:44 +0200

    test config

---
+++ config/colorscheme.nix
@@ -0,0 +1,5 @@
+{ self, ... }: {
+
+colorschemes.catppuccin.enable = true;
+
+}
---
+++ config/default.nix
@@ -0,0 +1,7 @@
+{
+  # Import all your configuration modules here
+  imports = [
+    ./default-plugins.nix
+    ./colorscheme.nix
+  ];
+}
